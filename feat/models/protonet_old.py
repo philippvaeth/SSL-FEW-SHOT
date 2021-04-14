fffc59a25f0b08c4a73caa66c99400f8c330ff6d
Philipp Vaeth <philipp.vaeth@yahoo.de>
2021-04-14 11:02:00 +0000

    added glvq to protonet

---
+++ feat/models/protonet_old.py
@@ -0,0 +1,29 @@
+import torch.nn as nn
+from feat.utils import euclidean_metric
+import torch
+class ProtoNet(nn.Module):
+
+    def __init__(self, args):
+        super().__init__()
+        self.args = args
+        if args.model_type == 'ConvNet':
+            from feat.networks.convnet import ConvNet
+            self.encoder = ConvNet()
+        elif args.model_type == 'ResNet':
+            from feat.networks.resnet import ResNet
+            self.encoder = ResNet()
+        elif args.model_type == 'AmdimNet':
+            from feat.networks.amdimnet import AmdimNet
+            model = AmdimNet(ndf=args.ndf, n_rkhs=args.rkhs, n_depth=args.nd)
+            self.encoder = model #torch.nn.DataParallel(model, device_ids=[ torch.device("cuda:0"), torch.device("cuda:1")])
+        else:
+            raise ValueError('')
+
+        #GLVQ
+        # 1. randaug in loader - data shot augmentieren = orig_datashot + randaug_datashot
+
+    def forward(self, data_shot, data_query):
+        proto = self.encoder(data_shot)
+        proto = proto.reshape(self.args.shot, self.args.way, -1).mean(dim=0)
+        logits = euclidean_metric(self.encoder(data_query), proto) / self.args.temperature
+        return logits
